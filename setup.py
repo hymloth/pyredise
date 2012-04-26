f8463203195ee5459a56ee1740ffda4503c64269
hymloth <hymloth@hymloth-desktop.(none)>
2012-04-26 19:12:57 +0300

    decreased index size by ~ 40%

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ str_check = Extension('pyredise.stringcheck', ['pyredise/fast_regex/stringcheck.
 
        
 setup (name = 'pyredise',
-       version = '0.1',
+       version = '0.11',
        packages=['pyredise'],
        description = 'Python Redis Search Engine',
        url = "http://github.com/hymloth/pyredise",
