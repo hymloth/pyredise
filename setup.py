12d4382a9fd07c8d28e90ad8411c148783619d20
hymloth <hymloth@hymloth-desktop.(none)>
2012-04-30 00:02:09 +0300

    updated version to 0.12

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ str_check = Extension('pyredise.stringcheck', ['pyredise/fast_regex/stringcheck.
 
        
 setup (name = 'pyredise',
-       version = '0.11',
+       version = '0.12',
        packages=['pyredise'],
        description = 'Python Redis Search Engine',
        url = "http://github.com/hymloth/pyredise",
