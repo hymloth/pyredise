70ce5aa1dc2225c9c1b1f206472962821a150677
hymloth <hymloth@hymloth-desktop.(none)>
2013-02-19 21:44:30 +0200

    v0.13

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ str_check = Extension('pyredise.stringcheck', ['pyredise/fast_regex/stringcheck.
 
        
 setup (name = 'pyredise',
-       version = '0.12',
+       version = '0.13',
        packages=['pyredise'],
        description = 'Python Redis Search Engine',
        url = "http://github.com/hymloth/pyredise",
