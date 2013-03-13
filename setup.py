d48f888cba216419bd8436275001cf92c99a6af2
hymloth <hymloth@hymloth-desktop.(none)>
2013-03-13 20:32:10 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ str_check = Extension('pyredise.stringcheck', ['pyredise/fast_regex/stringcheck.
        
 setup (name = 'pyredise',
        version = '0.14',
-       packages=['pyredise'],
+       packages=['pyredise', 'stemmer'],
        description = 'Python Redis Search Engine',
        url = "http://github.com/hymloth/pyredise",
-       download_url = "http://github.com/hymloth/pyredise/tarball/master",
+       download_url = "http://www.noowit.com/static/scripts/pyredise_0.14.zip",
        author = "Christos Spiliopoulos",
        author_email = "santos.koniordos@gmail.com",
        license = "Apache License, Version 2.0",
