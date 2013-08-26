bbc1c75201571580c4f33db0decc304c0f03de3c
hymloth <hymloth@hymloth-desktop.(none)>
2013-08-26 18:21:42 +0300

    support for more languages

--- pyredise/quick_language_detection.py
+++ pyredise/quick_language_detection.py
@@ -46,6 +46,7 @@ def _calculate_languages_ratios(words):
     # Compute per language included in nltk number of unique stopwords appearing in analyzed text
     for language in stopwords.fileids():
         stopwords_set = set(stopwords.words(language))
+        print language, stopwords_set
         words_set = set(words)
         common_elements = words_set.intersection(stopwords_set)
 
